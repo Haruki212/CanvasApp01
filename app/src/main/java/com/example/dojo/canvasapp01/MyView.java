96210ec3a9f4c746dc9da4d010bc51574e8521b6
Haruki212 <qqe59q59k804@docomo.ne.jp>
2019-02-23 16:43:24 +0900

    背景色つけたった。

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -1,6 +1,8 @@
 package com.example.dojo.canvasapp01;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class MyView extends View {
@@ -8,6 +10,13 @@ public class MyView extends View {
     //コンストラクタ（初期設定）
     public MyView(Context con) {
         super(con);
+    }
+    //　描画の処理
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        //　背景
+        canvas.drawColor(Color.MAGENTA);
     }
 
 }
