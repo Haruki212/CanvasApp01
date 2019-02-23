27e459da00cd0118e9fdbb8b1bbad77bbbdecba5
Haruki212 <qqe59q59k804@docomo.ne.jp>
2019-02-23 16:59:32 +0900

    円の色をつけた。

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -3,13 +3,17 @@ package com.example.dojo.canvasapp01;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
+    private Paint paint;
 
     //コンストラクタ（初期設定）
     public MyView(Context con) {
         super(con);
+
+        paint = new Paint();
     }
     //　描画の処理
     @Override
@@ -17,6 +21,15 @@ public class MyView extends View {
 
         //　背景
         canvas.drawColor(Color.MAGENTA);
+
+        //円
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(400,500,100,paint);
+        paint.setColor(Color.CYAN);
+        canvas.drawCircle(400,700,100,paint);
+
     }
 
 }
